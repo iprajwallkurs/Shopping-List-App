1fb2d6c104e85935510562263adfe20537b95aba
Prajwal L K Urs <153601054+iprajwallkurs@users.noreply.github.com>
2025-11-06 07:24:53 +0530

    added maps dependencies to build.gradle(app).kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,17 @@ android {
 
 dependencies {
 
+    implementation("com.google.maps.android:maps-compose:2.1.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.navigation:navigation-compose:2.6.0")
+
+
+
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
