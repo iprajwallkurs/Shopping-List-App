0b3ad6c72005be07f71f5a689dc2010e20b9288d
Prajwal L K Urs <153601054+iprajwallkurs@users.noreply.github.com>
2025-11-17 07:13:04 +0530

    Added userlocation and camerapositionstate to LocationSelectionScreen.kt

--- app/src/main/java/com/example/myshoppinglistapp/LocationSelectionScreen.kt
+++ app/src/main/java/com/example/myshoppinglistapp/LocationSelectionScreen.kt
@@ -1,6 +1,25 @@
 package com.example.myshoppinglistapp
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
-fun
+fun LocationSelectionScreen(
+    location: LocationData,
+    onLocationSelected: (LocationData) -> Unit){
+
+    val userlocation = remember{
+        mutableStateOf(LatLng(location.latitude, location.longitude))
+    }
+
+    val cameraPositionState = rememberCameraPositionState{
+        position = CameraPosition.fromLatLngZoom(userlocation.value, 10f)
+    }
+
+
+
+}
