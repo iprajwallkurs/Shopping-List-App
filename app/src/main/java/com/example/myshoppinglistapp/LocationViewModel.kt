3173b52102356f7d336a08754a08d397cb29f0e8
Prajwal L K Urs <153601054+iprajwallkurs@users.noreply.github.com>
2025-11-18 06:39:20 +0530

    Added viewModel to file LocationViewModel.kt

--- app/src/main/java/com/example/myshoppinglistapp/LocationViewModel.kt
+++ app/src/main/java/com/example/myshoppinglistapp/LocationViewModel.kt
@@ -1,4 +1,15 @@
 package com.example.myshoppinglistapp
 
-class LocationViewModel {
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.State
+
+
+class LocationViewModel : ViewModel(){
+    private val _location = mutableStateOf<LocationData?>(null)
+    val location: State<LocationData?> = _location
+
+    fun updateLocation(newLocation: LocationData){
+        _location.value = newLocation
+    }
 }
